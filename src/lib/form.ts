fae1d5eeb0c05d6d64779e660e541090884e388d
Ivan Avilov <ivan.avilov.0606@mail.ru>
2025-12-02 11:30:31 +0300

    Add form function

---
+++ src/lib/form.ts
@@ -0,0 +1,50 @@
+import Input from "./Input";
+
+export function form(element: HTMLFormElement) {
+  const inputs = element.querySelectorAll("input");
+  const inputMap = new Map<string, HTMLInputElement>();
+
+  for (const input of inputs) {
+    if (!input.name) {
+      throw new Error("Each input element must have a name attribute!");
+    }
+
+    if (!input.labels || input.labels.length === 0) {
+      throw new Error("Each input element must have at least one label tag!");
+    }
+
+    const describedBy = input.getAttribute("aria-describedby");
+
+    if (!describedBy) {
+      throw new Error(
+        "Each input element must have an aria-describedby attribute!"
+      );
+    }
+
+    if (!document.querySelector(`span#${describedBy}`)) {
+      throw new Error("Each input element must have connected described span!");
+    }
+
+    inputMap.set(input.name, input);
+  }
+
+  return {
+    field(name: string) {
+      const input = inputMap.get(name);
+
+      if (!input) {
+        throw new Error("There is no input with this name!");
+      }
+
+      return new Input(input);
+    },
+
+    validate() {
+      element.addEventListener("submit", (ev) => {
+        ev.preventDefault();
+
+        // TODO: добавить сбор данных
+      });
+    },
+  };
+}
